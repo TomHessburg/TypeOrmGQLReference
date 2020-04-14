07531a2e4ecae8f633861a12fcfec26bbfee05d7
Thomas Hessburg <44884308+TomHessburg@users.noreply.github.com>
2020-04-14 13:03:30 -0400

    unfortunately looks like theres some depricated stuff with using this. Have also been reading that gql yoga has had some issue recently. going to play atound with type graphql now

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,27 @@
 import "reflect-metadata";
 import { GraphQLServer } from "graphql-yoga";
 import { importSchema } from "graphql-import";
-import { createConnection } from "typeorm";
 import * as path from "path";
-import { resolvers } from "./resolvers";
+import * as fs from "fs";
+import { mergeSchemas, makeExecutableSchema } from "graphql-tools";
+import { GraphQLSchema } from "graphql";
+import { createConnection } from "typeorm";
 
-const typeDefs = importSchema(path.join(__dirname, "./schema.graphql"));
-
-const server = new GraphQLServer({ typeDefs, resolvers });
 // doing "createConnection" to sync db tables
 export const startServer = async () => {
+  const schemas: GraphQLSchema[] = [];
+  const folders = fs.readdirSync(path.join(__dirname, "./modules"));
+  folders.forEach((folder) => {
+    const { resolvers } = require(`./modules/${folder}/resolvers`);
+    const typeDefs = importSchema(
+      path.join(__dirname, `./modules/${folder}/schema.graphql`)
+    );
+    schemas.push(makeExecutableSchema({ resolvers, typeDefs }));
+  });
+
+  const server = new GraphQLServer({
+    schema: mergeSchemas({ subschemas: schemas }),
+  });
   await createConnection();
   await server.start();
   console.log("Server is running on localhost:4000");
